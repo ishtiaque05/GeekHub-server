2fd660839684b813715553507f423c7dde5bada3
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-08 00:49:20 +0600

    User authentication implemented - dotenv and bluebird added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
 import auth from './routes/auth';
 
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost/bookworm', { useMongoClient: true });
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
 app.use('/api/auth', auth);
 
