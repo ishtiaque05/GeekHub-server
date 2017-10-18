552edb777b847635108e213e72019385ac98a4a8
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-18 22:37:51 +0600

    Send confirmation email after signup - setup mailertrap.io - generate confirmation email token - setup nodemailer (yarn add nodemailer) - TODO:: handle mail failure in future

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/User';
 import parseErrors from '../utils/parseErrors';
+import { sendConfirmationEmail } from '../mailer';
 
 const router = express.Router();
 
@@ -8,11 +9,15 @@ router.post('/',(req, res)=> {
   const { email, password } = req.body.user;
   const user = new User({ email });
   user.setPassword(password);
+  user.setConfirmationToken();
   //  err object contains lot of info but we want
   // to extract only required part for which
   // we need parser and thus we created utils/parseErrors.js
   user.save()
-    .then( userRecord => res.json({  user: userRecord.toAuthJSON()  }))
+    .then( userRecord => {
+      sendConfirmationEmail(userRecord);
+      res.json({ user: userRecord.toAuthJSON() })
+    })
     .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
 })
 
