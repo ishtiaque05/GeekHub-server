4123731a0bdd8e23035e8254c98507976a06166e
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-10-27 14:04:40 +0600

    Fetch book pages from goodreads API show - added goodreads key in env variable - parse page type string to integer

--- src/routes/books.js
+++ src/routes/books.js
@@ -11,41 +11,37 @@ router.use(authenticate);
 
 router.get("/search", (req, res) => {
   request.get(
-    `https://www.goodreads.com/search/index.xml?key=5qGMRvMqhwVsLDynZ9B8Q&q=${req.query.q}`
+    `https://www.goodreads.com/search/index.xml?key=${process.env.GOODREADS_KEY}&q=${req.query.q}`
   ).then(result => parseString(result, (err, goodreadsResult)=> res.json({
     books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
       work =>({
         goodreadsId: work.best_book[0].id[0]._,
         title: work.best_book[0].title[0],
         authors: work.best_book[0].author[0].name[0],
-        covers: [work.best_book[0].image_url[0]]        
+        covers: [work.best_book[0].image_url[0]]
       })
     )
   })));
-  // res.json({
-  //   books: [
-  //     {
-  //       goodreadsId: 1,
-  //       title: "1984",
-  //       authors: "Orwell",
-  //       covers: [
-  //         "https://images.gr-assets.com/books/13489905661/5470.jpg",
-  //         "https://images.gr-assets.com/books/15046119571/9577857.jpg"
-  //       ],
-  //       pages: 198
-  //     },
-  //     {
-  //       goodreadsId: 2,
-  //       title: "Three Men in a Boat",
-  //       authors: "Jerome K. Jerome",
-  //       covers: [
-  //         "https://images.gr-assets.com/books/13927916561/4921.jpg",
-  //         "https://images.gr-assets.com/books/13120368781/627830.jpg"
-  //       ],
-  //       pages: 256
-  //     }
-  //   ]
-  // })
+});
+
+router.get("/fetchPages", (req, res) => {
+  const goodreadsId = req.query.goodreadsId;
+
+  request.get(
+    `https://www.goodreads.com/book/show.xml?key=${process.env
+      .GOODREADS_KEY}&id=${goodreadsId}`
+  )
+  .then(
+    result =>
+    parseString(result, (err, goodreadsResult)=> {
+      const numPages = goodreadsResult.GoodreadsResponse.book[0].num_pages[0]
+      const pages = numPages ? parseInt(numPages, 10) : 0;
+        res.json({
+          pages
+        })
+      }
+    )
+  );
 });
 
 export default router;
